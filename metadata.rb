bd0f38be5f13051a089873ebc63ba962419cf16c
Cassiano Leal <cassianoleal@gmail.com>
2013-06-30 22:02:26 -0300

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email "cl@cassianoleal.com"
 license          'Apache 2.0'
 description      'Manages deploy keys on SaaS VCS repositories'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
