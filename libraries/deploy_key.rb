0b64eefb7a2e89096e2621f95aed00ae567030d0
Alexander Merkulov <sasha@merqlove.ru>
2013-11-07 04:19:57 +0400

    version

--- libraries/deploy_key.rb
+++ libraries/deploy_key.rb
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+Gem::Uninstaller.remove('httparty')
 begin
   Gem::Specification.find_by_name('httparty')
 rescue Gem::LoadError
